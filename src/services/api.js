1d1684da6eea595f253459a4b4715a74aa6b828d
GustavoAbreuuu <gustavo.abreu2009@gmail.com>
2026-02-17 22:11:43 -0300

    chore(build): migrate project from Create React App to Vite
    Replaces react-scripts with Vite for faster build and dev server. Updates package.json scripts. Moves index.html to root. Configures vite.config.js. Updates environment variables to import.meta.env.

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { langColors } from './config'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export const getUser = async (login) => api.get(`/users/${login}`);
